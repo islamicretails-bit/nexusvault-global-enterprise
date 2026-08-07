acae5055e92a4afce20a23c55e610019fc1f07ab
islamicretails-bit <islamicretails@gmail.com>
2026-08-07 22:32:27 +0500

    feat: enterprise multi-module auto-commit

--- src/components/admin/LiveTrafficMap.ts
+++ src/components/admin/LiveTrafficMap.ts
@@ -4,24 +4,18 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
-
-interface TrafficData {
-  id: number;
-  latitude: number;
-  longitude: number;
-  country: string;
-  city: string;
-  trafficVolume: number;
-}
+import { GeoLocation } from '../types/index';
 
 interface LiveTrafficMapProps {
-  // Add props if needed
+  height: number;
+  width: number;
 }
 
-const LiveTrafficMap: React.FC<LiveTrafficMapProps> = () => {
-  const [trafficData, setTrafficData] = useState<TrafficData[]>([]);
-  const [mapCenter, setMapCenter] = useState<[number, number]>([37.7749, -122.4194]);
+const LiveTrafficMap: React.FC<LiveTrafficMapProps> = ({ height, width }) => {
+  const [mapCenter, setMapCenter] = useState<[number, number]>([51.505, -0.09]);
   const [zoom, setZoom] = useState(13);
+  const [trafficData, setTrafficData] = useState<GeoLocation[]>([]);
+  const [markerIcon, setMarkerIcon] = useState<L.Icon>();
 
   useEffect(() => {
     const fetchTrafficData = async () => {
@@ -32,38 +26,39 @@ const LiveTrafficMap: React.FC<LiveTrafficMapProps> = () => {
         console.error(error);
       }
     };
-    fetchTrafficData();
-  }, []);
 
-  const icon = L.icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    tooltipAnchor: [16, -28],
-  });
+    fetchTrafficData();
+
+    const icon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+    });
+
+    setMarkerIcon(icon);
+  }, []);
 
   return (
     <MapContainer
       center={mapCenter}
       zoom={zoom}
-      style={{ height: '600px', width: '100%' }}
+      style={{ height: `${height}px`, width: `${width}px` }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      {trafficData.map((traffic) => (
+      {trafficData.map((location, index) => (
         <Marker
-          key={traffic.id}
-          icon={icon}
-          position={[traffic.latitude, traffic.longitude]}
+          key={index}
+          icon={markerIcon}
+          position={[location.latitude, location.longitude]}
         >
           <Popup>
-            <h2>
-              {traffic.city}, {traffic.country}
-            </h2>
-            <p>Traffic Volume: {traffic.trafficVolume}</p>
+            <b>Location:</b> {location.city}, {location.country}
+            <br />
+            <b>Visits:</b> {location.visits}
           </Popup>
         </Marker>
       ))}
